a21e1b7b42185b4f3490ad9722ba7bf4711632f5
williamlardier <william.lardier@scality.com>
2022-11-24 11:03:55 +0100

    ARTESCA-6419: implement base http(s) agents and export

---
+++ lib/https-agent.ts
@@ -0,0 +1,39 @@
+import * as https from 'https';
+import clientConfigurationDefault from './config/default';
+
+/**
+ * @class ClientHttps
+ * Abstracts the native https.Agent class to enforce common
+ * networking configuration across components.
+ */
+ export default class ClientHttps extends https.Agent {
+    /**
+     * The maximum socket configuration defaults to 50.
+     */
+    private static maxSocketsConfiguration = Number(process.env.MAX_SOCKETS) || 50;
+
+    /**
+     * Constructor for the ClientHttps class
+     *
+     * @param opts - Custom HTTPs Agent options
+     * @param config - user-defined default configuration to apply
+     */
+    constructor(
+        opts?: https.AgentOptions,
+        config: clientConfigurationDefault = {
+            maxSockets: true,
+        },
+    ) {
+        // Enforce TCP session reuse configuration, unless explicitely specified.
+        const defaultConfigurations: https.AgentOptions = {};
+        if (config.maxSockets) {
+            defaultConfigurations.keepAlive = true;
+            defaultConfigurations.maxSockets = ClientHttps.maxSocketsConfiguration;
+            defaultConfigurations.maxFreeSockets = ClientHttps.maxSocketsConfiguration;
+        }
+        super({
+            ...opts,
+            ...defaultConfigurations,
+        });
+    }
+}
