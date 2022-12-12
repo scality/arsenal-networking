fed919ae16d6bc3de95a4f472b96205a6994fcae
williamlardier <william.lardier@scality.com>
2022-12-12 22:09:10 +0100

    ARSNN-1: export common configuration

---
+++ lib/config/maxSockets.ts
@@ -0,0 +1,19 @@
+import * as http from 'http';
+
+/**
+ * The maximum socket configuration defaults to 50.
+ */
+const maxSocketsNumber = Number(process.env.MAX_SOCKETS) || 50;
+
+/**
+ * The default configuration for the maxSocket profile.
+ * The goal is to enforce a `maxSockets` property to properly
+ * handle load.
+ */
+const maxSocketsConfiguration: http.AgentOptions  = {
+    keepAlive: true,
+    maxSockets: maxSocketsNumber,
+    maxFreeSockets: maxSocketsNumber,
+}
+
+export default maxSocketsConfiguration;
